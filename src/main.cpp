e98e0b598ab199fba5f582b11131e978e3b3d12e
Jef Frank <jef.frank@hypgames.com>
2025-02-16 08:27:27 -0600

    Hello World.  Running on basic hardware.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
+
 void setup() {
-// write your initialization code here
+    cli();
+    _PROTECTED_WRITE(CLKCTRL.OSCHFCTRLA, CLKCTRL_FREQSEL_24M_gc);
+    sei();
+
+    Serial.begin(115200);
+    Serial.println("Hello!");
 }
 
 void loop() {
